bbd4ff82f36d7a4975386446fa3b7623626daa06
Ahmed <wxwf30@gmail.com>
2023-07-11 10:37:27 +0300

    change all js code in js file and small changes in the index and style files

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,42 @@
-//make a varibales
-const cInput = document.getElementById("contentArea");
+list = [];
+const result = document.getElementById("showLists");
+const btn = document.getElementById("btn");
+const currentInputElement = document.getElementById("contentArea");
 
-const table = document.getElementById("showLists");
-function getText(){
-    
-    const textToAdd = document.createElement("span");
-
-    //to avoid empty value
-    if(cInput.value != ""){
-        textToAdd.innerHTML = cInput.value;
-    }else{
-        return
-    }
-
-    //in order to put the text in side the table i needed to sorrounded with <tr><td> [] </td></tr>  
-    const tableRow = document.createElement("tr");
-    const td =  document.createElement("td");
-
-    td.append(textToAdd);
-    tableRow.append(td);
-    table.prepend(tableRow);
-    
-    //delete the text from the the input area 
-    cInput.value = "";
+function getCurrentInputElementText() {
+    return currentInputElement.value;
 }
 
+function createRow() {
+    return "<tr><td><span>" + getCurrentInputElementText()
+        + "</span></td></tr>";
+}
+
+function inserRow() {
+    if (currentInputElement.value == "") {
+        return
+    } else {
+        list.push(createRow());
+    }
+
+}
+
+function updateTable() {
+    result.innerHTML = "";
+    list.map((myList) => {
+        result.innerHTML += myList;
+    });
+}
+
+function update() {
+    inserRow();
+    updateTable();
+    currentInputElement.value = "";
+}
+
+addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("btn").onclick();
+    }
+});
